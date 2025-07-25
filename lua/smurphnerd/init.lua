aa91e6720706530833f7010c5bd4a96f54c1c2d4
Sean Murphy <smurph005@gmail.com>
2025-07-25 14:44:57 +1000

    bug: rename primeagent to smurphnerd

--- lua/smurphnerd/init.lua
+++ lua/smurphnerd/init.lua
@@ -1,6 +1,6 @@
-require("theprimeagen.set")
-require("theprimeagen.remap")
-require("theprimeagen.lazy_init")
+require("smurphnerd.set")
+require("smurphnerd.remap")
+require("smurphnerd.lazy_init")
 
 -- DO.not
 -- DO NOT INCLUDE THIS
--- lua/smurphnerd/lazy_init.lua
+++ lua/smurphnerd/lazy_init.lua
@@ -12,6 +12,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "theprimeagen.lazy",
+    spec = "smurphnerd.lazy",
     change_detection = { notify = false }
 })
